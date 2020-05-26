4dc4333583bde203878b7802977a5b60eea5eded
Khaled Nasr <khaled.n.leila@gmail.com>
2020-05-26 05:03:04 +0200

    strange behaviour

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -6,7 +6,7 @@ const WEATHER_API_KEY = '07350895619f406cb145eea1f45a26cc'
 const PIXABAY_URL = 'https://pixabay.com/api/?key={api}&category=places&q={city}&image_type=photo&pretty=true'
 const PIXABAY_API_KEY = '16738849-0b85338957004177a866bb1c5'
 
-let endPoint = {}
+let endPoint = {};
 
 
 function handleSubmit(event) {
@@ -28,13 +28,13 @@ const getGeoData = async (city) => {
         const data = await response.json();
         console.log(data);
         console.log(data.geonames[0]['countryName']);
-        endPoint = {
-            "city": city,
-            "countryName": data.geonames[0]['countryName'],
-            "lat": data.geonames[0]["lat"],
-            "lng": data.geonames[0]["lng"]
-        }
-        return city;
+        console.log(data.geonames[0]['lat']);
+        console.log(data.geonames[0]['lng']);
+
+        endPoint["city"] = city;
+        endPoint["countryName"] = data.geonames[0]['countryName'];
+        endPoint["lat"]= data.geonames[0]["lat"];
+        endPoint["lng"]= data.geonames[0]["lng"];
     } catch (e) {
         console.log('error getting the data from geonames')
     }
@@ -44,19 +44,19 @@ const getWeatherData = async (city,days) => {
     let url = "";
     let index = 0;
     if (days < 7){
-         url = CURR_WEATHER_URL.replace("{city}", 'cairo').replace("{api}", WEATHER_API_KEY);
+         url = CURR_WEATHER_URL.replace("{city}", city).replace("{api}", WEATHER_API_KEY);
     }else{
-         url = FUT_WEATHER_URL.replace("{city}", 'cairo').replace("{api}", WEATHER_API_KEY).replace('{days}', days);
-         let index = days <= 16 ? days - 1 : 15;
+         index = days <= 16 ? days - 1 : 15;
+         url = FUT_WEATHER_URL.replace("{city}", city).replace("{api}", WEATHER_API_KEY).replace('{days}', index);
     }
     const response = await fetch(url)
 
     try {
         const data = await response.json()
-        console.log(data);
+        console.log("here" + data['data'][index]['temp']);
+        console.log("here" + data['data'][index]['weather']['description']);
         endPoint['temp'] = data['data'][index]['temp'];
-        endPoint['description'] = data[data][index]['weather']['description'];
-        return city;
+        endPoint['description'] = data['data'][index]['weather']['description'];
     } catch (event) {
         console.log("Error getting the  weather data")
     }
@@ -72,6 +72,7 @@ const getPixabayPhoto = async (city) => {
         console.log(data);
         console.log(data['hits'][0]["webformatURL"]);
         let imgURL = data['hits'][0]["webformatURL"].replace('_640','_340');
+        console.log(imgURL);
         endPoint['imgURL'] = imgURL;
     } catch (event) {
         console.log("Error getting the img")
@@ -83,18 +84,12 @@ const getDateDiff = (fDate) => {
     let a = new Date();
     let currDate = new Date(a.getFullYear(), a.getMonth(), a.getDate());
     let dateArray = fDate.split('-');
-    console.log(dateArray);
     const futDate = new Date(dateArray[0], dateArray[1] - 1, dateArray[2]);
-    console.log(currDate);
-    console.log(futDate);
-    console.log(futDate - currDate);
     const diffTime = Math.abs(futDate - currDate);
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    console.log(diffDays + " days");
+    console.log(diffDays);
     endPoint["diffDays"] = diffDays;
     return diffDays;
-
-
 }
 
 function setDateStartLimit() {
@@ -105,15 +100,13 @@ function setDateStartLimit() {
         day: a.getDate() >= 10 ? a.getDate() : ('0' + a.getDate())
     }
     let startDate = d.year + "-" + d.month + "-" + d.day;
-    console.log(startDate);
     document.getElementById('date').setAttribute("min", startDate);
 }
 
 const updateUI = async () => {
 
     document.getElementById('img').setAttribute('src', endPoint['imgURL']);
-    document.getElementById('result-div').innerHTML = `<p>${endPoint['city']}, ${endPoint['countryName']} is ${endPoint['diffDays']} away.</p><p>Typical weather for then is:\nTemperature: ${endPoint['temp']}\n${endPoint['description']}</p>`
-
+    document.getElementById('result-div').innerHTML = `<p>${endPoint['city']}, ${endPoint['countryName']} is ${endPoint['diffDays']} days away.</p><p>Typical weather for then is:\nTemperature: ${endPoint['temp']}\n${endPoint['description']}</p>`
 }
 
 function removeTrip(){
@@ -121,4 +114,4 @@ function removeTrip(){
     document.getElementById('result-div').innerHTML = "";
 }
 
-export{handleSubmit,removeTrip,setDateStartLimit}
+export{handleSubmit,removeTrip,setDateStartLimit, endPoint}
